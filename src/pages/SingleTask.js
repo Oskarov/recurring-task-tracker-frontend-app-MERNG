e9b7dbdcad9310d524f20f2ce492d46868a89d2c
Di <elven.legacy@hotmail.com>
2021-01-24 16:09:20 +0300

    SingleTask: fetch post mechanism fixed

--- src/pages/SingleTask.js
+++ src/pages/SingleTask.js
@@ -1,29 +1,33 @@
-import React, {useState} from 'react'
+import React from 'react'
 import gql from "graphql-tag";
 import {useQuery} from '@apollo/react-hooks';
 
 const SingleTask = (props) => {
 
-    const [post, setPost] = useState({});
 
     const postId = props.match.params.postId;
-    const {data} = useQuery(FEATCH_TASK_QUERY, {
+    const {loading, data} = useQuery(FETCH_TASK_QUERY, {
         variables: {
             postId
         },
-        update(proxy, {data}) {
-           setPost(data.getPost);
-        },
+        onError(ApolloError) {
+            console.log(ApolloError);
+        }
     });
 
     let postMarkup;
-    if (!post) {
+    if (loading) {
         postMarkup = <p>Загрузка...</p>
     } else {
-       const {id, body, createdAt, updatedAt, username} = post;
+       const {id, body, createdAt, updatedAt, username} = data.getPost;
+       postMarkup = <div>
+           {createdAt}{body}
+       </div>
     }
+
+
     return(
-        <div>
+        <div className="1245678">
             {postMarkup}
         </div>
     )
@@ -31,7 +35,7 @@ const SingleTask = (props) => {
 
 export default SingleTask;
 
-const FEATCH_TASK_QUERY = gql`
+const FETCH_TASK_QUERY = gql`
     query($postId: ID!){
         getPost(postId: $postId){
             id
